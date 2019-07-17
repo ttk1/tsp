95e969cadff278e0b03f2110aa1cf510a77f21dc
tama <tama@ttk1.net>
2019-07-18 01:10:43 +0900

    piyo

--- dist/hoge.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const hoge = 123;
-const piyo = 456;
-const fuga = [1, 2, 3, 4, 5, 6];
-const fugafuga = fuga.find(x => x == 2);
-console.log(fugafuga);
---
+++ src/piyo.ts
@@ -0,0 +1,3 @@
+export const piyo = () => {
+    console.log('piyo');
+}
